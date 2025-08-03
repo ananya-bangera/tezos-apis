9013107b516976ad5928ce57cbc4c7099b3fcb74
Sarvagnya Purohit <saRvaGnyA@users.noreply.github.com>
2025-08-03 17:00:49 +0530

    update fills schema

--- model/fills.js
+++ model/fills.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const FillsSchema = new mongoose.Schema({
   hash: String,
-  status: { type: String, enum: ['OPEN','PLACED', 'VALID', 'INVALID', 'COMPLETED', 'REFUNDED'], required: true },
+  status: { type: String, enum: ['OPEN', 'ACTIVE', 'PLACED', 'VALID', 'INVALID', 'COMPLETED', 'REFUNDED'], required: true },
   secret: String, // CAUTION: Don't expose this in APIs!
   srcEscrowDeployContractHash: String,
   dstEscrowDeployContractHash: String,
